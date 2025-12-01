fa0a4fdba9ae6de0a49421618c6c74020c30e713
Yanni Ma <yanni.ma@gusto.com>
2025-12-01 11:27:51 -0600

    got password of 5635 and it was too low

---
+++ day1pt2.rb
@@ -0,0 +1,29 @@
+lines = File.readlines("day1input.txt")
+
+dial = 50
+password = 0
+
+lines.each do |line|
+  amount = line[1..-1].to_i
+  if line.match(/R/)
+    dial += amount
+  else
+    dial -= amount
+  end
+  times = dial / 100
+  if times.negative?
+    times.abs
+    times += 1
+  end
+
+  password += times.abs
+
+  dial %= 100
+
+  if dial.zero?
+    password += 1
+  end
+end
+
+puts password
+
